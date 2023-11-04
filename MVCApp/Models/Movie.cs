3a9e2336f51c383476120a3e82b48cb94cf9a626
david <david@DESKTOP-M81T9R4>
2023-11-04 13:31:24 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+namespace MVCApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 1)]
+        [Required]
+        public string? Title { get; set; }
+        [Display(Name = "Realese Date")]
+        [DataType(DataType.Date)]
+        public DateTime RealeseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(1,100)]
+        [Required]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string? Rating { get; set; }
+
+    }
+}
---
+++ MVCApp/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVCApp.Data;
+using System;
+using System.Linq;
+
+
+namespace MVCApp.Models
+{
+    public static class SeedData
+    {
+        public static void Inicialize(IServiceProvider serviceProvider)
+        {
+
+            using (var context = new MVCAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MVCAppContext>>()))
+
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Spider Man",
+                        RealeseDate = DateTime.Parse("1999-10-01"),
+                        Genre = "Action",
+                        Price = 3.9M,
+                        Rating = "PG-13"
+                    },
+                    new Movie
+                    {
+                        Title = "Spider Man 2",
+                        RealeseDate = DateTime.Parse("2001-7-1"),
+                        Genre = "Action",
+                        Price = 9.9M,
+                        Rating = "NA"
+                    },
+                    new Movie
+                    {
+                        Title = "Capitan American",
+                        RealeseDate = DateTime.Parse("2007-2-20"),
+                        Genre = "Adventure",
+                        Price = 7.2M,
+                        Rating = "PI-11"
+                    },
+                    new Movie
+                    {
+                        Title = "El Pianista",
+                        RealeseDate = DateTime.Parse("2005-7-19"),
+                        Genre = "Action",
+                        Price = 1.2M,
+                        Rating = "PG-13"
+                    }
+                    );
+                context.SaveChanges();
+            }
+
+
+        }
+    }
+}
